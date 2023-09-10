5e709ae6c563fcc32bdf2e8b4ec1660377278bf3
Daniel Oaks <daniel@danieloaks.net>
2023-09-10 15:28:51 +1000

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { BhSetupOptions } from "./interface/birdhouse.setup.options";
 import { BhRegistrationRequests } from "./module/registration";
 
-export default class BHApi {
+export default class BhApi {
   constructor(private readonly options: BhSetupOptions) { }
 
   public registration: BhRegistrationRequests = new BhRegistrationRequests(this.options);
